0dd324db25dbbfd456ad4a9e5fcf7a3e949ad5fc
Sahana TH <ssahana8791@gmail.com>
2026-05-02 10:11:46 +0530

    Add deployment config files

--- run.py
+++ run.py
@@ -1,18 +1,8 @@
-# run.py — Entry point to start the Flask server
-# Run this with: python run.py
-
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    print("🚀 DoseAI Backend starting...")
-    print("📡 Server running at: http://localhost:5000")
-    print("🔍 Test it: http://localhost:5000/medicine/ibuprofen")
-    print("Press CTRL+C to stop.\n")
-    
-    app.run(
-        debug=True,   # Auto-reloads when you save code changes
-        port=5000,
-        host="0.0.0.0"
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
